26412aa9316f2e36a3e8f786743c8b5068a28151
Dirk Kok <the@dirkkok.nl>
2021-09-10 17:19:37 +0200

    fixed issue in publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dotnet build
+dotnet build -c Release
 dotnet pack Foxite.Common/Foxite.Common.csproj
 dotnet pack Foxite.Common.Core/Foxite.Common.Core.csproj
 dotnet nuget push -s $(cat pushrepo.txt) -k $(cat pushkey.txt) $(ls -t Foxite.Common/bin/Release/*.nupkg | head -n 1)
