2e02039440aedb35870eb92e929951b86cd28d18
Dirk Kok <the@dirkkok.nl>
2021-09-17 21:44:20 +0200

    fix publish.sh if there are no changes

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@
 dotnet build -c Release
 dotnet pack Foxite.Common/Foxite.Common.csproj
 dotnet pack Foxite.Common.Core/Foxite.Common.Core.csproj
-dotnet nuget push -s $(cat pushrepo.txt) -k $(cat pushkey.txt) $(ls -t Foxite.Common/bin/Release/*.nupkg | head -n 1)
-dotnet nuget push -s $(cat pushrepo.txt) -k $(cat pushkey.txt) $(ls -t Foxite.Common.Core/bin/Release/*.nupkg | head -n 1)
+dotnet nuget push --skip-duplicate -s $(cat pushrepo.txt) -k $(cat pushkey.txt) $(ls -t Foxite.Common/bin/Release/*.nupkg | head -n 1)
+dotnet nuget push --skip-duplicate -s $(cat pushrepo.txt) -k $(cat pushkey.txt) $(ls -t Foxite.Common.Core/bin/Release/*.nupkg | head -n 1)
 
